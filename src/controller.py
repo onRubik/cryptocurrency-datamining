5d406eb37b55a7f1beae0a1bfc779c765b7d0b05
rubik <lifesonrubik@gmail.com>
2023-01-31 15:41:21 -0600

    new features

--- src/controller.py
+++ src/controller.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import RandomForestClassifier
 from binance import Client
 import pandas as pd
 import os
@@ -51,3 +52,22 @@ class controller:
         con.commit()
 
         con.close
+
+    
+    def sklearnExample(delf):
+
+        test_data = pd.read_csv("/kaggle/input/titanic/test.csv")
+
+        y = train_data_no_mods["Survived"]
+
+        features = ["Pclass", "Sex", "SibSp", "Parch"]
+        X = pd.get_dummies(train_data_no_mods[features])
+        X_test = pd.get_dummies(test_data[features])
+
+        model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+        model.fit(X, y)
+        predictions = model.predict(X_test)
+
+        output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+        output.to_csv('submission.csv', index=False)
+        print('csv file saved')
---
+++ src/db_table_example.sql
@@ -0,0 +1,10 @@
+CREATE TABLE btcbusd_4h_historical (
+    time   DATETIME PRIMARY KEY
+                    NOT NULL,
+    open   DECIMAL  NOT NULL,
+    high   DECIMAL  NOT NULL,
+    low    DECIMAL  NOT NULL,
+    close  DECIMAL  NOT NULL,
+    volume DECIMAL  NOT NULL
+)
+WITHOUT ROWID;

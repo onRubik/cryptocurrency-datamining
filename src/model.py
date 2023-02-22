439c96e14bcea91434ef8e6b08946c40b0cf6962
rubik <lifesonrubik@gmail.com>
2023-02-22 14:03:37 -0600

    test paths

--- src/model.py
+++ src/model.py
@@ -9,10 +9,12 @@ class mainModel:
         lookback_frame = 'years'
         lookback_utc_adjustment = 'UTC-7'
         lookback_string = lookback_val + ' ' + lookback_frame + ' ago ' + lookback_utc_adjustment
+        image_output_name = 'image.png'
 
-        newController = controller(symbol, interval, lookback_string)
-        newController.sqlUpdate()
-        newController.lstmModel()
+        newController = controller(symbol, interval, lookback_string, image_output_name)
+        # newController.sqlUpdate()
+        # newController.lstmModel()
+        
 
 
 if __name__ == "__main__":
